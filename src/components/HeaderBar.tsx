858212e389fc7b54fd9076aeafa850692c079f82
Ivan Krstev <ivankrstev246@gmail.com>
2024-07-08 17:17:12 +0200

    Changed the closing sidebar btn to handle closing with attribute change

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,26 +1,11 @@
 "use client";
-import Image from "next/image";
+import CloseSidebarButton from "@/components/CloseSidebarButton";
 
-export default function HeaderBar({
-  setIsSidebarOpen,
-}: {
-  setIsSidebarOpen: (isOpen: boolean) => void;
-}) {
+export default function HeaderBar() {
   return (
     <header className="flex items-center bg-slate-300 p-3 dark:bg-slate-800">
       <div className="block md:hidden">
-        <button
-          onClick={() => setIsSidebarOpen(true)}
-          className="rounded hover:bg-gray-800 focus:outline-none"
-        >
-          <Image
-            className="filter-black dark:filter-white"
-            src="/icons/menu_FILL0_wght400_GRAD0_opsz24.svg"
-            alt="Menu Icon"
-            width={32}
-            height={32}
-          />
-        </button>
+        <CloseSidebarButton />
       </div>
     </header>
   );
