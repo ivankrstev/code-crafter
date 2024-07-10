0c4d164cc14ab9654bf6c5c190673fe1ea27439c
Ivan Krstev <ivankrstev246@gmail.com>
2024-07-10 03:01:00 +0200

    Added scrolling prevention & removed modal shadow

--- src/components/ModalBackdrop.tsx
+++ src/components/ModalBackdrop.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 
 export default function ModalBackdrop({
   children,
@@ -8,17 +8,22 @@ export default function ModalBackdrop({
   children?: React.ReactNode;
   closeModal: () => void;
 }) {
+  useEffect(() => {
+    // Prevent scrolling when modal is open
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
   return (
     <Fragment>
       <div
         id="modal-backdrop"
         className="fixed inset-0 z-40 bg-black bg-opacity-50 backdrop-blur-sm transition-all"
-        onClick={() => {
-          closeModal();
-          console.log("clicked");
-        }}
+        onClick={() => closeModal()}
       />
-      <div className="fixed inset-0 left-1/2 top-1/2 z-50 flex h-fit w-fit max-w-[90vw] -translate-x-1/2 -translate-y-1/2 rounded-lg shadow-lg shadow-gray-300 transition-all duration-500 dark:shadow-slate-600">
+      <div className="fixed inset-0 left-1/2 top-1/2 z-50 flex h-fit w-fit max-w-[90vw] -translate-x-1/2 -translate-y-1/2 rounded-lg transition-all duration-500 dark:shadow-slate-600">
         {children}
       </div>
     </Fragment>
