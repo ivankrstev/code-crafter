087f28968e9b25f6696a106f1d84b48ca5ace5b0
Ivan Krstev <ivankrstev246@gmail.com>
2024-10-17 01:09:58 +0200

    feat: add a page for not selected snippet

--- src/app/snipboard/[id]/page.tsx
+++ src/app/snipboard/[id]/page.tsx
@@ -1,8 +1,10 @@
-export default function SnippetsPage({ params }: { params: { id: string } }) {
-  const { id } = params;
+export default function NoSnippetSelectedPage() {
   return (
-    <div>
-      <p>Vault id: {id}</p>
+    <div className="flex h-full items-center justify-center">
+      <div className="mb-[10em] rounded-lg bg-form px-10 py-12 text-center shadow-xl">
+        <h2 className="mb-2 text-2xl font-bold">No Snippet Selected</h2>
+        <p>Please select a snippet from the sidebar.</p>
+      </div>
     </div>
   );
 }
