2e0dfdc1eb24483ee3a2c6933d5b4945cec7e50a
Ivan Krstev <ivankrstev246@gmail.com>
2024-10-17 00:11:18 +0200

    style: changed header padding & set close sidebar button to be centered

--- src/components/CloseSidebarButton.tsx
+++ src/components/CloseSidebarButton.tsx
@@ -2,24 +2,24 @@
 import Image from "next/image";
 
 export default function CloseSidebarButton() {
-    return (
-      <button
-        onClick={() => {
-          const sidebar = document.querySelector("#sidebar")!;
-          sidebar.setAttribute(
-            "data-open",
-            sidebar.getAttribute("data-open") !== "true" ? "true" : "false",
-          );
-        }}
-        className="rounded hover:bg-gray-800 focus:outline-none"
-      >
-        <Image
-          className="filter-black dark:filter-white"
-          src="/icons/menu_FILL0_wght400_GRAD0_opsz24.svg"
-          alt="Menu Icon"
-          width={32}
-          height={32}
-        />
-      </button>
-    );
+  return (
+    <button
+      onClick={() => {
+        const sidebar = document.querySelector("#sidebar")!;
+        sidebar.setAttribute(
+          "data-open",
+          sidebar.getAttribute("data-open") !== "true" ? "true" : "false",
+        );
+      }}
+      className="center rounded hover:bg-gray-800 focus:outline-none"
+    >
+      <Image
+        className="filter-black dark:filter-white"
+        src="/icons/menu_FILL0_wght400_GRAD0_opsz24.svg"
+        alt="Menu Icon"
+        width={32}
+        height={32}
+      />
+    </button>
+  );
 }
--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -3,8 +3,8 @@ import CloseSidebarButton from "@/components/CloseSidebarButton";
 
 export default function HeaderBar() {
   return (
-    <header className="flex items-center bg-slate-300 p-3 dark:bg-slate-800">
-      <div className="block md:hidden">
+    <header className="flex items-center bg-slate-300 dark:bg-slate-800">
+      <div className="block p-1 md:hidden">
         <CloseSidebarButton />
       </div>
     </header>
