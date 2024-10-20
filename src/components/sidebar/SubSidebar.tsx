d89a1e4edd934c3a2cd181cd36cf04acdc12efc7
Ivan Krstev <ivankrstev246@gmail.com>
2024-10-21 00:55:16 +0200

    feat: add fetching & rendering of snippets with caching to sub sidebar

--- src/components/sidebar/SubSidebar.tsx
+++ src/components/sidebar/SubSidebar.tsx
@@ -1,3 +1,11 @@
+import CreateSnippetModal from "@/components/CreateSnippetModal";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import { Snippet } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+import { Fragment, useState } from "react";
+import useSWR from "swr";
+
 interface SubSidebarProps {
   selectedVault: string | null;
   onBackClick: () => void;
@@ -9,21 +17,66 @@ export default function SubSidebar({
   onBackClick,
   visible,
 }: SubSidebarProps) {
+  const [createSnippetModalOpen, setCreateSnippetModalOpen] =
+    useState<boolean>(false);
+
+  const fetchSnippets = async (selectedVault: string) =>
+    await fetch(`/api/snippets/${selectedVault}`).then((res) => res.json());
+  const { data, error } = useSWR(`snippets/${selectedVault}`, () =>
+    fetchSnippets(`${selectedVault}`),
+  );
+
   return (
     <div className={`h-full w-full space-y-2 ${visible ? "" : "hidden"}`}>
       <button onClick={onBackClick} className="block hover:text-gray-300">
-        ← Back
+        {"<- Back"}
       </button>
-      <div className="pt-2">{selectedVault} Details</div>
-      <a href="#" className="block hover:text-gray-300">
-        Detail 1
-      </a>
-      <a href="#" className="block hover:text-gray-300">
-        Detail 2
-      </a>
-      <a href="#" className="block hover:text-gray-300">
-        Detail 3
-      </a>
+      <div>
+        <div className="flex items-center justify-between">
+          <p className="text-xl font-semibold">{data?.vaultName}</p>
+          <button
+            className="rounded-lg p-1 transition-colors duration-200 hover:bg-slate-200 dark:hover:bg-gray-600"
+            title="New Snippet"
+            onClick={() => setCreateSnippetModalOpen(true)}
+          >
+            <Image
+              className="filter-black dark:filter-white"
+              src="/icons/add_FILL0_wght400_GRAD0_opsz24.svg"
+              alt="New"
+              width={20}
+              height={20}
+            />
+          </button>
+        </div>
+        {error && <div>Failed to load</div>}
+        {!data && <LoadingSpinner />}
+        {data && (
+          <div className="mt-3 max-h-[calc(100vh-200px)] min-h-[100px] space-y-2 overflow-y-auto">
+            {data.snippets.length === 0 && (
+              <p>No snippets found in this vault.</p>
+            )}
+            {data.snippets.length > 0 && (
+              <Fragment>
+                {data.snippets.map((snippet: Snippet) => (
+                  <Link
+                    key={snippet.id}
+                    href={`/snipboard/${selectedVault}/${snippet.id}`}
+                    className="block hover:text-gray-300"
+                    title={snippet.title}
+                  >
+                    {snippet.title}
+                  </Link>
+                ))}
+              </Fragment>
+            )}
+          </div>
+        )}
+      </div>
+      {createSnippetModalOpen && (
+        <CreateSnippetModal
+          closeModal={() => setCreateSnippetModalOpen(false)}
+        />
+      )}
     </div>
   );
 }
