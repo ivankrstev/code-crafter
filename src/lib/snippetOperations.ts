1ef5d3892d64e581179fdd4f8b18af0d658f17a2
Ivan Krstev <ivankrstev246@gmail.com>
2024-11-08 00:40:21 +0100

    feat: add delete snippet operation

--- src/lib/snippetOperations.ts
+++ src/lib/snippetOperations.ts
@@ -49,3 +49,27 @@ export const handleTitleChangeInSidebar = async (
     });
   } else toast.error("Failed to update snippet title");
 };
+
+export const deleteSnippet = async (
+  vaultId: string,
+  snippetId: string,
+): Promise<boolean> => {
+  try {
+    const res = await fetch(`/api/snippets/${vaultId}/${snippetId}`, {
+      method: "DELETE",
+    });
+    if (res.ok) {
+      mutate(`/api/snippets/${vaultId}`, (data: any) => {
+        return {
+          ...data,
+          snippets: data.snippets.filter(
+            (snippet: Snippet) => snippet.id !== snippetId,
+          ),
+        };
+      });
+      return true;
+    } else return false;
+  } catch (error) {
+    return false;
+  }
+};
