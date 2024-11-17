018eb6dc719e36ac1ca1e889e8bd622d9feb43e2
Ivan Krstev <ivankrstev246@gmail.com>
2024-11-17 01:07:53 +0100

    feat: add new option for redirecting to /welcome if no user name in token

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,16 @@
+import { JWT } from "next-auth/jwt";
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
 export default withAuth(
   function middleware(req) {
-    if (req.nextauth.token) return NextResponse.next();
-    return NextResponse.redirect("/signin");
+    // Redirect to /signin if the user is not authenticated
+    if (!req.nextauth.token) return NextResponse.redirect("/signin");
+    // Redirect to /welcome if the user is authenticated but doesn't have a name
+    if (!(req.nextauth.token as JWT).name)
+      return NextResponse.rewrite(new URL("/welcome", req.url));
+    // Continue if the user is authenticated and has a name
+    return NextResponse.next();
   },
   {
     callbacks: {
