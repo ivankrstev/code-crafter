a129c11032a5c50287e2f7faebac8b8fb2cee6ea
Ivan Krstev <ivankrstev246@gmail.com>
2024-11-20 01:18:55 +0100

    style: change not selected vault/snippet pages status to appear as boxes

--- src/app/snipboard/[id]/page.tsx
+++ src/app/snipboard/[id]/page.tsx
@@ -1,7 +1,7 @@
 export default function NoSnippetSelectedPage() {
   return (
     <div className="flex h-full items-center justify-center">
-      <div className="mb-[10em] rounded-lg bg-form px-10 py-12 text-center shadow-xl">
+      <div className="rounded-lg border border-dashed border-black px-10 py-12 text-center dark:border-white">
         <h2 className="mb-2 text-2xl font-bold">No Snippet Selected</h2>
         <p>Please select a snippet from the sidebar.</p>
       </div>
--- src/app/snipboard/page.tsx
+++ src/app/snipboard/page.tsx
@@ -1,9 +1,9 @@
 export default function SnippetsPage() {
   return (
     <div className="flex h-full items-center justify-center">
-      <div className="rounded-lg bg-form px-10 py-12 text-center shadow-xl">
+      <div className="rounded-lg border border-dashed border-black px-10 py-12 text-center dark:border-white">
         <h2 className="mb-2 text-2xl font-bold">No Vault Selected</h2>
-        <p>Please select a vault to view its code snippets.</p>
+        <p>Please select a vault from the sidebar.</p>
       </div>
     </div>
   );
